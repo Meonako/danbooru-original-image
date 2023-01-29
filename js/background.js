b3b87c7ab6db03877e3065fac5af36604c629384
Meonako <76484203+Meonako@users.noreply.github.com>
2023-01-30 00:23:52 +0700

    Remove: Debug code

--- js/background.js
+++ js/background.js
@@ -5,14 +5,10 @@ chrome.tabs.onUpdated.addListener(( TabId, changeInfo, Tab ) => {
     // For chrome
     if (Tab.url.startsWith('chrome')) return;
 
-    console.log(Tab)
-
     // Not danbooru
     if (!Tab.url.includes('danbooru')) return;
 
     if (changeInfo.status != 'complete') return;
-
-    console.log(`Executing Script on: ${Tab.url}`)
 
     chrome.scripting.executeScript({
         target: { tabId: TabId },
