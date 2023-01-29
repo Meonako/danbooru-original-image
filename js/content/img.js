9aea64a608e986b7e39b13f63fae527e64002921
Meonako <76484203+Meonako@users.noreply.github.com>
2023-01-30 00:49:23 +0700

    Fixed: Different image broken
    Manually changing from `sample` to `original`
    was stupid idea to begin with
    
    This fixed when image has different format and broke the page
    (aka different url path)

--- js/content/img.js
+++ js/content/img.js
@@ -1,4 +1,15 @@
 function main() {
+    // 'Resized to xx% of original (view original)' Notice
+    const resizeNotice = document.querySelector(
+        'div[id="image-resize-notice"]'
+    );
+
+    if (resizeNotice) {
+        // Set 'Resized to xx% of original (view original)' to `display: none`
+        // Why? Because it is easier to fix if user found it not working
+        resizeNotice.style = "display: none;";
+    }
+
     // Select img#image / img[id="image"]
     const imageElement = document.querySelector('img[id="image"]');
 
@@ -8,27 +19,36 @@ function main() {
         return;
     }
 
-    // Change to original size
-    imageElement.src = imageElement.src
-        .replace("sample", "original")
-        .replace("sample-", "");
+    let imageUrl;
+
+    // If `resizeNotice` is found, grab original img url
+    // If not, try changing manually
+    if (!resizeNotice) {
+        // Change to original size
+        imageUrl = Fallback(imageElement);
+    } else {
+        const anchor = resizeNotice.lastElementChild;
+
+        // If not found anchor, fallback to manual
+        if (!anchor) {
+            imageUrl = Fallback(imageElement);
+        } else {
+            imageUrl = anchor.href;
+        }
+    }
+
+    // Changing src to original image URL
+    imageElement.src = imageUrl;
 
     // Remove 'width' and 'height' attr which will keep the image to be specify size
     imageElement.removeAttribute("width");
     imageElement.removeAttribute("height");
+}
 
-    // Suppress 'Resized to xx% of original (view original)'
-    const resizeNotice = document.querySelector(
-        'div[id="image-resize-notice"]'
-    );
-
-    // If not found, stop execution without log cuz it's not important (lol)
-    if (!resizeNotice) {
-        return;
-    }
-
-    // Remove 'Resized to xx% of original (view original)' element
-    resizeNotice.remove();
+function Fallback(img) {
+    // Replace `sample` with `original` (parent path)
+    // Replace `sample-` with empty string (image file name)
+    return img.src.replace("sample", "original").replace("sample-", "");
 }
 
 // Call function to execute
